9f85c18368078a68a6d799b0039bac21f0616cd3
lam-van-duc <93573446+lam-van-duc@users.noreply.github.com>
2024-01-16 18:08:00 +0700

    Ducvl

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#027BE3',
+        lightPrimary: '#32A3F4',
+      },
+    },
+  },
+  plugins: [],
+};
