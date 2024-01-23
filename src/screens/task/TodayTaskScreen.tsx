f22e013840f541bdab26654adb4251b92365cc0b
lam-van-duc <lamduc21122003@gmail.com>
2024-01-23 16:51:19 +0700

    Duclv

--- src/screens/task/TodayTaskScreen.tsx
+++ src/screens/task/TodayTaskScreen.tsx
@@ -1,19 +1,9 @@
-import {
-  Dimensions,
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import React from 'react';
 import AppHeader from '../../components/navigators/AppHeader';
 import Lightbox from 'react-native-lightbox-v2';
-import {useNavigation} from '@react-navigation/native';
-const WINDOW_WIDTH = Dimensions.get('window').width;
-const BASE_PADDING = 10;
+import {ViewPagerZoom} from 'react-native-image-zoom';
 const TodayTaskScreen = () => {
-  const navigator = useNavigation();
   const renderImage = () => (
     <Image
       source={{
@@ -30,72 +20,24 @@ const TodayTaskScreen = () => {
       <View>
         <Lightbox underlayColor="white">
           <Image
-            style={styles.contain}
+            className="w-full h-80"
             resizeMode="contain"
             source={{
               uri: 'https://www.yayomg.com/wp-content/uploads/2014/04/yayomg-pig-wearing-party-hat.jpg',
             }}
           />
         </Lightbox>
+
+        <Image
+          source={{
+            uri: 'https://www.yayomg.com/wp-content/uploads/2014/04/yayomg-pig-wearing-party-hat.jpg',
+            headers: {
+              Referer: 'http://...',
+            },
+          }}></Image>
       </View>
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: BASE_PADDING,
-  },
-  closeButton: {
-    color: 'white',
-    borderWidth: 1,
-    borderColor: 'white',
-    padding: 8,
-    borderRadius: 3,
-    textAlign: 'center',
-    margin: 10,
-    alignSelf: 'flex-end',
-  },
-  customHeaderBox: {
-    height: 150,
-    backgroundColor: '#6C7A89',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    marginLeft: -BASE_PADDING,
-    marginRight: -BASE_PADDING,
-  },
-  col: {
-    flex: 1,
-  },
-  square: {
-    width: WINDOW_WIDTH / 2,
-    height: WINDOW_WIDTH / 2,
-    justifyContent: 'center',
-    alignSelf: 'center',
-  },
-  squareFirst: {
-    backgroundColor: '#C0392B',
-  },
-  squareSecond: {
-    backgroundColor: '#019875',
-  },
-  squareText: {
-    textAlign: 'center',
-    color: 'white',
-  },
-  carousel: {
-    height: WINDOW_WIDTH - BASE_PADDING * 2,
-    width: WINDOW_WIDTH - BASE_PADDING * 2,
-    backgroundColor: 'white',
-  },
-  contain: {
-    flex: 1,
-    height: 150,
-  },
-  text: {
-    marginVertical: BASE_PADDING * 2,
-  },
-});
+
 export default TodayTaskScreen;
