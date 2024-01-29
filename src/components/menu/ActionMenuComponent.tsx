ddaa6d6bb0217b9cb4e7dbd9f3e8594e1dcbd90b
lam-van-duc <lamduc21122003@gmail.com>
2024-01-29 11:58:58 +0700

    Duclv

---
+++ src/components/menu/ActionMenuComponent.tsx
@@ -0,0 +1,33 @@
+import {Alert, Animated, Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {COLORS} from '../../../constans/colors';
+// import {
+//   Menu,
+//   MenuOption,
+//   MenuOptions,
+//   MenuProvider,
+//   MenuTrigger,
+// } from 'react-native-popup-menu';
+import {Icon, TouchableRipple} from 'react-native-paper';
+import ActionSheet from 'react-native-actionsheet';
+
+const ActionMenuComponent = () => {
+  //     const showActionSheet = () => {
+  //      this.ActionSheet.show();
+  //    };
+  //     return (
+  //       <View>
+  //         <Text onPress={this.showActionSheet}>Open ActionSheet</Text>
+  //         <ActionSheet
+  //           ref={o => this.ActionSheet = o}
+  //           title={'Which one do you like ?'}
+  //           options={['Apple', 'Banana', 'cancel']}
+  //           cancelButtonIndex={2}
+  //           destructiveButtonIndex={1}
+  //           onPress={(index) => { /* do something */ }}
+  //         />
+  //       </View>
+  //     );
+};
+
+export default ActionMenuComponent;
